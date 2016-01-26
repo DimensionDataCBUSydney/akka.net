4bb1c6c112faef6759844cd0c7ceff44a2d06068
tintoy <tintoy@tintoy.io>
2016-01-26 23:07:15 +1100

    Patch package dependencies during build.

--- build.fsx
+++ build.fsx
@@ -325,13 +325,13 @@ module Nuget =
     let getAkkaDependency project =
         match project with
         | "Akka" -> []
-        | "Akka.Cluster" -> ["DD.Signed.Akka.Remote", release.NugetVersion]
-        | "Akka.Persistence.TestKit" -> ["DD.Signed.Akka.Persistence", preReleaseVersion; "DD.Signed.Akka.TestKit.Xunit2", release.NugetVersion]
-        | persistence when (persistence.Contains("Sql") && not (persistence.Equals("Akka.Persistence.Sql.Common"))) -> ["DD.Signed.Akka.Persistence.Sql.Common", preReleaseVersion]
-        | persistence when (persistence.StartsWith("Akka.Persistence.")) -> ["DD.Signed.Akka.Persistence", preReleaseVersion]
-        | "Akka.DI.TestKit" -> ["DD.Signed.Akka.DI.Core", release.NugetVersion; "DD.Signed.Akka.TestKit.Xunit2", release.NugetVersion]
-        | di when (di.StartsWith("Akka.DI.") && not (di.EndsWith("Core"))) -> ["DD.Signed.Akka.DI.Core", release.NugetVersion]
-        | testkit when testkit.StartsWith("Akka.TestKit.") -> ["DD.Signed.Akka.TestKit", release.NugetVersion]
+        | "Akka.Cluster" -> ["Akka.Remote", release.NugetVersion]
+        | "Akka.Persistence.TestKit" -> ["Akka.Persistence", preReleaseVersion; "Akka.TestKit.Xunit2", release.NugetVersion]
+        | persistence when (persistence.Contains("Sql") && not (persistence.Equals("Akka.Persistence.Sql.Common"))) -> ["Akka.Persistence.Sql.Common", preReleaseVersion]
+        | persistence when (persistence.StartsWith("Akka.Persistence.")) -> ["Akka.Persistence", preReleaseVersion]
+        | "Akka.DI.TestKit" -> ["Akka.DI.Core", release.NugetVersion; "Akka.TestKit.Xunit2", release.NugetVersion]
+        | di when (di.StartsWith("Akka.DI.") && not (di.EndsWith("Core"))) -> ["Akka.DI.Core", release.NugetVersion]
+        | testkit when testkit.StartsWith("Akka.TestKit.") -> ["Akka.TestKit", release.NugetVersion]
         | _ -> ["Akka", release.NugetVersion]
 
     // used to add -pre suffix to pre-release packages
@@ -373,7 +373,11 @@ let createNugetPackages _ =
         let releaseDir = projectDir @@ @"bin\Release"
         let packages = projectDir @@ "packages.config"
         let packageDependencies = if (fileExists packages) then (getDependencies packages) else []
-        let dependencies = packageDependencies @ getAkkaDependency project
+        let dependencies =
+            (packageDependencies @ getAkkaDependency project)
+            |> List.map(
+                fun (dependency, version) -> if (dependency.StartsWith("Akka")) then ("DD.Signed." + dependency, version) else (dependency, version)
+            )
         let releaseVersion = getProjectVersion project
 
         let pack outputDir symbolPackage =
